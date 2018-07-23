83a5e033e6d042061e6509928df972336a6c8ee1
WLAngel <akloratok@gmail.com>
2018-07-23 23:03:22 +0800

    config api gateway domain manager

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ function getEnv() {
   return {
     G_MAP_TABLE: process.env.G_MAP_TABLE,
     G_BUCKET: process.env.G_BUCKET,
+    CUSTOM_DOMAIN_NAME: process.env.CUSTOM_DOMAIN_NAME,
+    CERTIFICATE_NAME: process.env.CERTIFICATE_NAME,
   };
 }
 
